07cab8fb9f6eb524572e64d5cfb165709ce61a04
Niferu <wargacki.patryq@gmail.com>
2023-03-01 19:01:10 +0100

    verify and clearing code update

--- hardhat.config.js
+++ hardhat.config.js
@@ -32,12 +32,6 @@ module.exports = {
     solidity: {
         compilers: [
             {
-                version: "0.6.0",
-            },
-            {
-                version: "0.6.6",
-            },
-            {
                 version: "0.8.0",
             },
             {
@@ -58,11 +52,10 @@ module.exports = {
     },
     namedAccounts: {
         deployer: {
-            default: 0, // here this will by default take the first account as deployer
-            1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured,
-            // the account 0 on one network can be different than on another
+            default: 0,
+            1: 0,
         },
-        player: {
+        user: {
             default: 1,
         },
     },
