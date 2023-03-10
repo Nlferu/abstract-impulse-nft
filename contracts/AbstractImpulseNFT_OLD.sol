3e7d4750f2724922d8d2f2609b867ea780293c74
Niferu <wargacki.patryq@gmail.com>
2023-03-10 03:10:15 +0100

    new NFT standard implemented

--- contracts/AbstractImpulseNFT_OLD.sol
+++ contracts/AbstractImpulseNFT_OLD.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.8;
 
-import "erc721a/contracts/ERC721A.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 error Abstract__NotEnoughETH();
 error Abstract__TransferFailed();
-error Abstract__TotalSupplyReached();
 error Abstract__NotExistingTokenId();
 error Abstract__BiddingClosedForThisNFT();
 error Abstract__ContractOwnerIsNotAllowedToBid();
@@ -15,9 +15,10 @@ error Abstract__BiddingNotFinishedYetForThisNFT();
 /**
  *@dev
  * Add "ReentrancyGuard" to protect.
- * Check SOLMATE !!!
+ * Also ERC721 is cheaper -> but it doesn't have _setTokenURI function 3k Gas diff only
+ * Switch to ERC721A and check SOLMATE as ERC721 is old nowadays
  */
-contract AbstractImpulseNFT is ERC721A, Ownable {
+contract AbstractImpulseNFT_OLD is ERC721URIStorage, Ownable {
     // Type Declaration
     enum BiddingState {
         OPEN,
@@ -25,13 +26,12 @@ contract AbstractImpulseNFT is ERC721A, Ownable {
     }
 
     // NFT Variables
-    // Below is not necessary
-    uint256 private immutable MAX_SUPPLY = 300;
+    using Counters for Counters.Counter;
+    Counters.Counter private s_tokenId;
 
     // NFT Mappings
-    mapping(uint256 => string) private s_tokenURIs;
-    mapping(uint256 => uint256) private s_tokenIdToBids;
     mapping(uint256 => address payable) private s_tokenIdToBidder;
+    mapping(uint256 => uint256) private s_tokenIdToBids;
     mapping(uint256 => BiddingState) private s_tokenIdToBiddingState;
 
     // NFT Events
@@ -39,27 +39,30 @@ contract AbstractImpulseNFT is ERC721A, Ownable {
     event FirstNFTBidPlaced(uint256 amount);
     event NFTBidPlaced(uint256 amount);
 
-    constructor() ERC721A("Abstract Impulse", "AIN") {}
+    constructor() ERC721("Abstract Impulse", "AIN") {}
 
-    function mintNFT(string memory externalTokenURI, string memory nftTitle) public onlyOwner {
-        if ((totalSupply() + 1) >= MAX_SUPPLY) {
-            revert Abstract__TotalSupplyReached();
-        }
-        uint256 newTokenId = totalSupply();
+    function mintNFT(string memory tokenURI, string memory nftTitle) public onlyOwner returns (uint256) {
+        uint256 newTokenId = s_tokenId.current();
+        // This is to be tested compared to "s_tokenId += 1"
+        s_tokenId.increment();
         // Changing State For New NFT is not necessary as it is set to OPEN as default...
         // s_tokenIdToBiddingState[newTokenId] = BiddingState.OPEN;
 
-        _mint(msg.sender, 1);
-        s_tokenURIs[newTokenId] = externalTokenURI;
+        _mint(msg.sender, newTokenId);
+        // Below is just assigning "tokenId" to correct "tokenURI"
+        // We use below because of additional customization, but it is not most gas efficient
+        // Is it even necessary?
+        _setTokenURI(newTokenId, tokenURI);
 
         emit NFTMinted(msg.sender, nftTitle);
+        return newTokenId;
     }
 
     function placeBid(uint256 tokenId) public payable {
         if (msg.sender == owner()) {
             revert Abstract__ContractOwnerIsNotAllowedToBid();
         }
-        if (totalSupply() > tokenId) {
+        if (s_tokenId.current() > tokenId) {
             if (s_tokenIdToBidder[tokenId] == address(0)) {
                 if (msg.value == 0) {
                     revert Abstract__NotEnoughETH();
@@ -98,10 +101,6 @@ contract AbstractImpulseNFT is ERC721A, Ownable {
         }
     }
 
-    function tokenURI(uint256 tokenId) public view override returns (string memory) {
-        return s_tokenURIs[tokenId];
-    }
-
     // Function for owner to end bidding and transfer NFT immediately
     function acceptBid(uint256 tokenId) public onlyOwner {
         if (s_tokenIdToBiddingState[tokenId] == BiddingState.OPEN) {
@@ -128,7 +127,7 @@ contract AbstractImpulseNFT is ERC721A, Ownable {
 
     /**
      * @dev This will transfer NFT after it's bidding ends
-     * Function to be changed to allow auction winner to transfer token for himself with approve()
+     * Change it to approve() function to allow user claiming NFT
      */
     function tokenTransfer(uint256 tokenId) public onlyOwner {
         if (s_tokenIdToBiddingState[tokenId] == BiddingState.OPEN) {
@@ -152,6 +151,14 @@ contract AbstractImpulseNFT is ERC721A, Ownable {
         }
     }
 
+    function getTokenCounter() public view returns (Counters.Counter memory) {
+        return s_tokenId;
+    }
+
+    function getTokenIdToTokenURI(uint256 tokenId) public view returns (string memory) {
+        return this.tokenURI(tokenId);
+    }
+
     function getBiddingState(uint256 tokenId) public view returns (BiddingState) {
         return s_tokenIdToBiddingState[tokenId];
     }
@@ -164,7 +171,7 @@ contract AbstractImpulseNFT is ERC721A, Ownable {
         return s_tokenIdToBidder[tokenId];
     }
 
-    function getBid(uint256 tokenId) public view returns (uint256) {
+    function getBidd(uint256 tokenId) public view returns (uint256) {
         return s_tokenIdToBids[tokenId];
     }
 
