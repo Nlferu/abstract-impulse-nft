ef82ca6feee0dac492b230320c921c272debd9b3
Niferu <wargacki.patryq@gmail.com>
2023-03-25 19:16:29 +0100

    scripts update

---
+++ scripts/frontEndScripts.js
@@ -0,0 +1,17 @@
+/**
+@dev List of scripts to be coded for frontEnd and backEnd calls
+
+Scripts:
+
+getHighestBidder(frontend)
+getHighestBid(frontend)
+safeTransferFrom(frontend)
+placeBid(frontend)
+
+mintNFT (backend)
+approve(backend)
+acceptBid(backend)
+withdrawMoney(backend)
+renewAuction(backend)
+
+*/
