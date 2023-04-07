cf09da42ffdec854409778a2f3170609c92367c6
Niferu <wargacki.patryq@gmail.com>
2023-04-07 12:42:03 +0200

    format update

--- deploy/99-deploy-front-end.js
+++ deploy/99-deploy-front-end.js
@@ -2,6 +2,7 @@ const { ethers } = require("hardhat")
 const frontEndContractsFile = "../no-patrick-code/constants/networkMapping.json"
 const frontEndAbiLocation = "../no-patrick-code/constants/"
 const fs = require("fs")
+
 module.exports = async function () {
     if (process.env.UPDATE_FRONT_END) {
         console.log("updating front end...")
@@ -9,13 +10,12 @@ module.exports = async function () {
         await updateAbi()
     }
 }
+
 async function updateAbi() {
     const abstractImpulseNft = await ethers.getContract("AbstractImpulseNFT")
-    fs.writeFileSync(
-        `${frontEndAbiLocation}AbstractImpulseNFT.json`,
-        abstractImpulseNft.interface.format(ethers.utils.FormatTypes.json)
-    )
+    fs.writeFileSync(`${frontEndAbiLocation}AbstractImpulseNFT.json`, abstractImpulseNft.interface.format(ethers.utils.FormatTypes.json))
 }
+
 async function updateContractAddresses() {
     const abstractImpulseNft = await ethers.getContract("AbstractImpulseNFT")
     const chainId = network.config.chainId.toString()
@@ -25,8 +25,9 @@ async function updateContractAddresses() {
             contractAddresses[chainId]["AbstractImpulseNFT"].push(abstractImpulseNft.address)
         }
     } else {
-        contractAddresses[chainId] = { "AbstractImpulseNFT": [abstractImpulseNft.address] }
+        contractAddresses[chainId] = { AbstractImpulseNFT: [abstractImpulseNft.address] }
     }
     fs.writeFileSync(frontEndContractsFile, JSON.stringify(contractAddresses))
 }
+
 module.exports.tags = ["all", "frontend"]
