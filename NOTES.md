bbd7fe5e0db3135bc3a6ffd9f12d5c70b5c4ca08
Niferu <wargacki.patryq@gmail.com>
2023-04-26 16:51:38 +0200

    added slither for security tests

--- NOTES.md
+++ NOTES.md
@@ -1,6 +1,4 @@
 
 ToDo:
 
-withdrawMoney -> decide on function visibility
-withdrawUnclaimedBids -> new function to be coded
-tests -> update after above
+
--- utils/verify.js
+++ utils/verify.js
@@ -1,7 +1,7 @@
 const { run } = require("hardhat")
 
 const verify = async (contractAddress, args) => {
-    console.log("Verifying contract...")
+    console.log("Verifying Contract...")
     try {
         await run("verify:verify", {
             address: contractAddress,
